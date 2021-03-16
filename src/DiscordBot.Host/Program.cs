5123a13b9f232f74a7720a9daa2f1ae0083b5e4c
Frans Freij <frans.freij@gmail.com>
2021-03-16 21:47:32 +0100

    Inital commit

---
+++ src/DiscordBot.Host/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Discord.Net; 
+
+
+using DiscordBot.Core; 
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult(); 
+
+        public async Task MainAsync()
+        {
+            // entry point
+        }
+    }
+}
